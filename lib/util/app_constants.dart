107c8aada6d856c0b5099e76653c35ab57e96750
Burak Kuyucu <kuyucuburakg@gmail.com>
2022-02-08 19:33:24 +0300

    change some files

--- lib/util/app_constants.dart
+++ lib/util/app_constants.dart
@@ -5,10 +5,10 @@ class AppConstants {
   static const bool debugModeStaticSprites = false;
   static const Anchor anchor = Anchor.center;
 
-  static const double mostTopLeftTileX = 250;
-  static const double mostTopLeftTileY = 250;
+  static const double mostTopLeftTileX = 200;
+  static const double mostTopLeftTileY = 200;
   static const double tileSize = 100; // This should be divisible by wallSize to build the map correctly.
-  static const double wallSize = 20;
+  static const double wallSize = 25;
 
   static const int tilePriority = 0;
   static const int wallPriority = 0;
