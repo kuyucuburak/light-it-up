ad2614de9bee79456abdce4613877479e321de67
Burak Kuyucu <kuyucuburakg@gmail.com>
2022-02-18 19:47:44 +0300

    dynamic sizes rather than static sizes

--- lib/util/app_constants.dart
+++ lib/util/app_constants.dart
@@ -7,12 +7,12 @@ class AppConstants {
   static const double mostTopLeftTileX = 200;
   static const double mostTopLeftTileY = 200;
 
-  static const double staticAnimationSize = 100;
-  static const double wireSize = 100; // This should be divisible by wallSize to build the map correctly.
-  static const double wallSize = 50;
+  static const double staticAnimationSize = 120;
+  static const double wireSize = 120; // This should be divisible by wallSize to build the map correctly.
+  static const double wallSize = 40;
 
-  static final Vector2 electricityHorizontalSize = Vector2(30, 75);
-  static final Vector2 electricityVerticalSize = Vector2(75, 30);
+  static final Vector2 electricityHorizontalSize = Vector2(wireSize * 0.2, wireSize * 0.75);
+  static final Vector2 electricityVerticalSize = Vector2(wireSize * 0.75, wireSize * 0.2);
 
   static const int staticAnimationPriority = 4;
   static const int dynamicAnimationPriority = 3;
--- lib/util/extensions/vector_extension.dart
+++ lib/util/extensions/vector_extension.dart
@@ -3,11 +3,11 @@ import 'package:light_it_up/component/sprite/wire.dart';
 import 'package:light_it_up/util/app_constants.dart';
 
 extension WireExtensions on Wire {
-  Vector2 get electricityLeftPos => Vector2(x - AppConstants.electricityHorizontalSize.x / 2, y);
+  Vector2 get electricityLeftPos => Vector2(x - AppConstants.wireSize * 0.4, y);
 
-  Vector2 get electricityRightPos => Vector2(x + AppConstants.electricityHorizontalSize.x / 2, y);
+  Vector2 get electricityRightPos => Vector2(x + AppConstants.wireSize * 0.4, y);
 
-  Vector2 get electricityBottomPos => Vector2(x, y + AppConstants.electricityVerticalSize.y / 2);
+  Vector2 get electricityBottomPos => Vector2(x, y + AppConstants.wireSize * 0.4);
 
-  Vector2 get electricityTopPos => Vector2(x, y - AppConstants.electricityVerticalSize.y / 2);
+  Vector2 get electricityTopPos => Vector2(x, y - AppConstants.wireSize * 0.4);
 }
